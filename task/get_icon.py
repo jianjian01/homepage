5934c72dc67c47202926f7b30f38dbab8c762239
gaojianjian <jianjian001@outlook.com>
2020-02-05 19:19:17 +0800

    update get icon

--- task/get_icon.py
+++ task/get_icon.py
@@ -23,43 +23,73 @@ headers = {
 }
 
 
+def download(host, icon):
+    """下载文件"""
+    icon_dir = '../static/site/'
+    headers['Host'] = host
+    print(icon)
+    try:
+        response = requests.get(icon, stream=True, timeout=5, headers=headers)
+        if response.status_code != 200:
+            return None
+    except Exception as e:
+        return None
+    icon_id = ''.join([random.choice(string.ascii_lowercase + string.digits) for _ in range(16)])
+    with open(os.path.join(icon_dir, '{}.png'.format(icon_id)), 'wb') as image:
+        for chunk in response.iter_content(1024):
+            image.write(chunk)
+    print(icon_id)
+    return icon_id
+
+
+def try_request(host):
+    """"""
+    if host.startswith('www.'):
+        schema_list = ['https://', 'http://']
+    else:
+        schema_list = ['https://', 'http://', 'http://www.', 'https://www.']
+    for schema in schema_list:
+        url = schema + host + '/favicon.ico'
+        icon_id = download(host, url)
+        if icon_id:
+            return icon_id
+
+
+def try_fetch(host):
+    """尝试解析首页，找到 icon 的地址"""
+    headers['Host'] = host
+    if host.startswith('www.'):
+        schema_list = ['https://', 'http://']
+    else:
+        schema_list = ['https://', 'http://', 'http://www.', 'https://www.']
+
+    for schema in schema_list:
+        url = schema + host
+        try:
+            icons = favicon.get(url, headers=headers, timeout=5)
+        except Exception as e:
+            continue
+        return icons
+    return ''
+
+
 @db_session
 def get_icon():
     """下载"""
-    icon_dir = '../static/site/'
-
     for site in Site.select(lambda x: not x.icon)[:1000]:
-        icons = []
-        print(site.host)
-        headers['Host'] = site.host
-        for url in ['https://', 'http://', 'http://www.', 'https://www.']:
-            if url.endswith('www.') and site.host.startswith('www.'):
-                continue
-            url = url + site.host
-            print(url)
-            try:
-                icons = favicon.get(url, headers=headers, timeout=5)
-            except Exception as e:
-                print(e)
-                continue
-            else:
-                url = url + site.host
-                break
-
+        host = site.host
+        icons = try_fetch(host)
+        if not icons and host.startswith('www.'):
+            icons = try_fetch(host[4:])
         if icons:
             icon = icons[0].url
+            icon_id = download(host, icon)
         else:
-            icon = "{}/{}".format(url, 'favicon.ico')
-        try:
-            response = requests.get(icon, stream=True, timeout=5)
-            if response.status_code != 200:
-                continue
-        except Exception as e:
+            # icons = "http://{}/{}".format(host, 'favicon.ico')
+            icon_id = try_request(host)
+
+        if not icon_id:
             continue
-        icon_id = ''.join([random.choice(string.ascii_lowercase + string.digits) for _ in range(16)])
-        with open(os.path.join(icon_dir, '{}.png'.format(icon_id)), 'wb') as image:
-            for chunk in response.iter_content(1024):
-                image.write(chunk)
         site.icon = icon_id
         commit()
         time.sleep(1)
@@ -82,7 +112,8 @@ def main():
     db.generate_mapping()
     while 1:
         get_icon()
-    # update_icon()
+        time.sleep(10)
+        update_icon()
 
 
 if __name__ == '__main__':
